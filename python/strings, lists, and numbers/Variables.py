b0ebfce44f691ba9f95c56d942d2bfbda52fd31b
Mcswiggles <139520055+Mcswiggles@users.noreply.github.com>
2023-07-19 19:37:22 -0500

    added strings and vars

---
+++ python/strings, lists, and numbers/Variables.py
@@ -0,0 +1,30 @@
+'''
+Can not start with a number
+
+No spaces in a variable
+
+Can't use any of these symbols
+: ' " , < > / ? | \ ( ) ! @ # $ % ^ & * ~ + -
+
+Vars are typically all lower case by best practies except in certain situations
+
+Avoid using words that have special meaning in Python like 'list' and 'str' 'int'
+'''
+
+a=5
+print(a)
+
+print(a+a)
+
+a=a*a
+print(a)
+
+print(type(a))
+
+a=30.1
+print(type(a))
+
+my_income=100
+tax_rate=0.1
+my_taxes=my_income*tax_rate
+print(my_taxes)
---
+++ python/strings, lists, and numbers/strings.py
@@ -0,0 +1,25 @@
+'''
+
+indexing
+h e l l o
+0 1 2 3 4
+
+reverse indexing
+h  e  l  l  o
+0 -4 -3 -2 -1
+
+slicing - sub secion of string
+[start:stop:step]
+
+'hello' or
+"hello"
+
+double quotes when ' is used in a sentence
+"I'm going on a run"
+'''
+
+print("hello world")
+print("hello\nworld")
+print("hello\tworld")
+
+print(len("oogabooga"))
